0e5d19c9d09b931427e80e3d911387c838686a7d
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-01-13 13:01:38 +1100

    Remove debugging

--- src/main.cpp
+++ src/main.cpp
@@ -25,34 +25,5 @@ int main()
 	auto port_ids = mesher_interface::get_surface_ids_from_coms(port_points);
 	auto port_surface_elements = mesher_interface::get_surface_elems_by_id(port_ids, node_map);
 
-	for (auto n : port_surface_elements[0])
-	{
-		for (auto node : n.nodes)
-		{
-			nodes[node].tag = 2;
-			std::cout << node << std::endl;
-		}
-	}
-
-	for (auto elem : volume_elements)
-	{
-		if (nodes[elem.nodes[0]].tag == 2)
-		{
-			std::cout << "Yes" << std::endl;
-		}
-		if (nodes[elem.nodes[1]].tag == 2)
-		{
-			std::cout << "Yes" << std::endl;
-		}
-		if (nodes[elem.nodes[2]].tag == 2)
-		{
-			std::cout << "Yes" << std::endl;
-		}
-		if (nodes[elem.nodes[3]].tag == 2)
-		{
-			std::cout << "Yes" << std::endl;
-		}
-	}
-
 	return 0;
 }
