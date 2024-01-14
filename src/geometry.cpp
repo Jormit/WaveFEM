a57f3a468011aee7308aaf42b3c63d780f7d1b15
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-01-14 15:16:57 +1100

    Seperated geometry.h into geometry.cpp

---
+++ src/geometry.cpp
@@ -0,0 +1,44 @@
+#include "geometry.h"
+
+box::box(double xmin, double ymin, double zmin, double xmax, double ymax, double zmax)
+	: xmin(xmin), ymin(ymin), zmin(zmin), xmax(xmax), ymax(ymax), zmax(zmax)
+{}
+
+void box::add_padding(double x, double y, double z)
+{
+	xmin -= x;
+	ymin -= y;
+	zmin -= z;
+	xmax += x;
+	ymax += y;
+	zmax += z;
+}
+
+std::ostream& operator << (std::ostream& os, box const& b) 
+{
+	return os
+		<< "xmin: " << b.xmin << " ymin: " << b.ymin
+		<< " zmin: " << b.zmin << " xmax: " << b.xmax
+		<< " ymax: " << b.ymax << " zmax: " << b.zmax;
+}
+
+node::node(std::array<double, 3> coords, int type_2d, int type_3d)
+	: coords(coords), type_2d(type_2d), type_3d(type_3d)
+{}
+
+tri::tri(std::array<size_t, 3> nodes, std::array<size_t, 3> edges, size_t face, size_t tag)
+	: nodes(nodes), edges(edges), face(face), tag(tag)
+{}
+
+std::array<size_t, 2> tri::get_edge_nodes(size_t edge) const 
+{
+	switch (edge) {
+	case 0: return { nodes[0], nodes[1] };
+	case 1: return { nodes[0], nodes[2] };
+	case 2: return { nodes[1], nodes[2] };
+	}
+}
+
+tet::tet(std::array<size_t, 4> nodes, std::array<size_t, 6> edges, std::array<size_t, 4> faces, size_t tag)
+	: nodes(nodes), edges(edges), faces(faces), tag(tag)
+{}
--- src/geometry.h
+++ src/geometry.h
@@ -10,26 +10,9 @@ class box
 public:
 	double xmin, ymin, zmin, xmax, ymax, zmax;
 
-	box(double xmin, double ymin, double zmin, double xmax, double ymax, double zmax) 
-		: xmin(xmin), ymin(ymin), zmin(zmin), xmax(xmax), ymax(ymax), zmax(zmax) 
-	{}
-
-	void add_padding(double x, double y, double z)
-	{
-		xmin -= x;
-		ymin -= y;
-		zmin -= z;
-		xmax += x;
-		ymax += y;
-		zmax += z;
-	}
-
-	friend std::ostream& operator << (std::ostream& os, box const& b) {
-		return os 
-			<< "xmin: " << b.xmin << " ymin: " << b.ymin 
-			<< " zmin: " << b.zmin << " xmax: " << b.xmax 
-			<< " ymax: " << b.ymax << " zmax: " << b.zmax;
-	}
+	box(double xmin, double ymin, double zmin, double xmax, double ymax, double zmax);
+	void add_padding(double x, double y, double z);
+	friend std::ostream& operator << (std::ostream& os, box const& b);
 };
 
 struct node
@@ -37,10 +20,7 @@ struct node
 	std::array<double, 3> coords;
 	int type_2d;
 	int type_3d;
-	node(std::array<double, 3> coords, int type_2d, int type_3d)
-		: coords(coords), type_2d(type_2d), type_3d(type_3d)
-	{}
-
+	node(std::array<double, 3> coords, int type_2d, int type_3d);
 	node() = default;
 };
 
@@ -50,17 +30,9 @@ struct tri
 	std::array<size_t, 3> edges;
 	size_t face;
 	size_t tag;
-	tri(std::array<size_t, 3> nodes, std::array<size_t, 3> edges, size_t face, size_t tag)
-		: nodes(nodes), edges(edges), face(face), tag(tag)
-	{}
 
-	std::array<size_t, 2> get_edge_nodes(size_t edge) const {
-		switch (edge) {
-		case 0: return { nodes[0], nodes[1] };
-		case 1: return { nodes[0], nodes[2] };
-		case 2: return { nodes[1], nodes[2] };
-		}
-	}	
+	tri(std::array<size_t, 3> nodes, std::array<size_t, 3> edges, size_t face, size_t tag);
+	std::array<size_t, 2> get_edge_nodes(size_t edge) const;
 };
 
 struct tet
@@ -68,8 +40,7 @@ struct tet
 	std::array<size_t, 4> nodes;
 	std::array<size_t, 6> edges;
 	std::array<size_t, 4> faces;
+
 	size_t tag;
-	tet(std::array<size_t, 4> nodes, std::array<size_t, 6> edges, std::array<size_t, 4> faces, size_t tag)
-		: nodes(nodes), edges(edges), faces(faces), tag(tag)
-	{}
+	tet(std::array<size_t, 4> nodes, std::array<size_t, 6> edges, std::array<size_t, 4> faces, size_t tag);
 };
