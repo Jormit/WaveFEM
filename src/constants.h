3d6b14712096347371c6805cb33eb4972c31a2ac
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-01-16 08:23:10 +1100

    Update constants

--- src/constants.h
+++ src/constants.h
@@ -1,6 +1,7 @@
 namespace constants
 {
-	const double ep_0 = 8.854e-12;
-	const double mu_0 = 4 * 3.141592653589793e-7;
+	const double pi = 3.141592653589793;
+	const double ep_0 = 8.854 * 1e-12;
+	const double mu_0 = 4 * pi * 1e-7;
 }
 
