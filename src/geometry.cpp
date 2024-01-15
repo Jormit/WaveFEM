d6e7780e3e16836701c2bb273652c57d93c3ab9b
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-01-15 21:00:49 +1100

    Point class

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -1,9 +1,5 @@
 #include "geometry.h"
 
-box::box(double xmin, double ymin, double zmin, double xmax, double ymax, double zmax)
-	: xmin(xmin), ymin(ymin), zmin(zmin), xmax(xmax), ymax(ymax), zmax(zmax)
-{}
-
 void box::add_padding(double x, double y, double z)
 {
 	xmin -= x;
@@ -22,14 +18,6 @@ std::ostream& operator << (std::ostream& os, box const& b)
 		<< " ymax: " << b.ymax << " zmax: " << b.zmax;
 }
 
-node::node(std::array<double, 3> coords, int type_2d, int type_3d)
-	: coords(coords), type_2d(type_2d), type_3d(type_3d)
-{}
-
-tri::tri(std::array<size_t, 3> nodes, std::array<size_t, 3> edges, size_t face, size_t tag)
-	: nodes(nodes), edges(edges), face(face), tag(tag)
-{}
-
 std::array<size_t, 2> tri::get_edge_nodes(size_t edge) const 
 {
 	switch (edge) {
@@ -39,13 +27,9 @@ std::array<size_t, 2> tri::get_edge_nodes(size_t edge) const
 	}
 }
 
-tet::tet(std::array<size_t, 4> nodes, std::array<size_t, 6> edges, std::array<size_t, 4> faces, size_t tag)
-	: nodes(nodes), edges(edges), faces(faces), tag(tag)
-{}
-
-std::vector<std::array<double,3>> generate_grid_points(box box, size_t num_x, size_t num_y, size_t num_z)
+std::vector<point> generate_grid_points(box box, size_t num_x, size_t num_y, size_t num_z)
 {
-	std::vector<std::array<double, 3>> points;
+	std::vector<point> points;
 	points.reserve(num_x * num_y * num_z);
 	for (int x = 0; x < num_x; x++)
 	{
--- src/geometry.h
+++ src/geometry.h
@@ -11,18 +11,22 @@ class box
 public:
 	double xmin, ymin, zmin, xmax, ymax, zmax;
 
-	box(double xmin, double ymin, double zmin, double xmax, double ymax, double zmax);
 	void add_padding(double x, double y, double z);
 	friend std::ostream& operator << (std::ostream& os, box const& b);
 };
 
+struct point
+{
+	double x;
+	double y;
+	double z;
+};
+
 struct node
 {
-	std::array<double, 3> coords;
+	point coords;
 	int type_2d;
 	int type_3d;
-	node(std::array<double, 3> coords, int type_2d, int type_3d);
-	node() = default;
 };
 
 struct tri
@@ -31,8 +35,6 @@ struct tri
 	std::array<size_t, 3> edges;
 	size_t face;
 	size_t tag;
-
-	tri(std::array<size_t, 3> nodes, std::array<size_t, 3> edges, size_t face, size_t tag);
 	std::array<size_t, 2> get_edge_nodes(size_t edge) const;
 };
 
@@ -41,9 +43,7 @@ struct tet
 	std::array<size_t, 4> nodes;
 	std::array<size_t, 6> edges;
 	std::array<size_t, 4> faces;
-
 	size_t tag;
-	tet(std::array<size_t, 4> nodes, std::array<size_t, 6> edges, std::array<size_t, 4> faces, size_t tag);
 };
 
-std::vector<std::array<double, 3>> generate_grid_points(box box, size_t num_x, size_t num_y, size_t num_z);
+std::vector<point> generate_grid_points(box box, size_t num_x, size_t num_y, size_t num_z);
