2515b92dd0bf73a4fb136b74a33119057d489947
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-01-19 21:38:12 +1100

    Ports object

--- src/setup.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include <string>
-#include "geometry.h"
-
-namespace setup
-{
-	std::vector<point> load_port_setup(std::string filename);
-}
--- src/sim.cpp
+++ src/sim.cpp
@@ -3,8 +3,9 @@
 
 #include <iostream>
 
-sim::sim(std::vector<node> nodes, std::vector<tet> volume_elems, std::vector<int> port_surface_ids, std::vector<std::vector<tri>> port_elems) :
-	nodes(nodes), volume_elems(volume_elems), port_surface_ids(port_surface_ids), port_elems(port_elems),
+
+sim::sim(std::vector<node> nodes, std::vector<tet> volume_elems, ports ports) :
+	nodes(nodes), volume_elems(volume_elems), sim_ports(ports),
 	port_eigen_vectors(), port_eigen_wave_numbers(), port_dof_maps()
 {}
 
@@ -13,14 +14,14 @@ void sim::solve_ports()
 	port_eigen_vectors.clear();
 	port_eigen_wave_numbers.clear();
 	port_dof_maps.clear();
-	for (int p = 0; p < port_elems.size(); p++)
+	for (int p = 0; p < sim_ports.elements.size(); p++)
 	{
-		auto dof_map = fem::_2d::mixed_order::dof_map(nodes, port_elems[p]);
+		auto dof_map = fem::_2d::mixed_order::dof_map(nodes, sim_ports.elements[p]);
 		port_dof_maps.push_back(dof_map);
 
 		Eigen::SparseMatrix<double> S;
 		Eigen::SparseMatrix<double> T;
-		std::tie(S, T) = fem::_2d::mixed_order::assemble_S_T(nodes, port_elems[p], dof_map);
+		std::tie(S, T) = fem::_2d::mixed_order::assemble_S_T(nodes, sim_ports.elements[p], dof_map);
 
 		Eigen::VectorXd values;
 		Eigen::MatrixXd vecs;
