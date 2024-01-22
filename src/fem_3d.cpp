7dd3b3d95cb8be133dda18233f4578955e2e2c92
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-01-22 22:15:07 +1100

    Progress on 3d basis functions

--- src/fem_3d.cpp
+++ src/fem_3d.cpp
@@ -10,7 +10,7 @@ Eigen::Matrix<double, 4, 4> fem::_3d::simplex_coefficients(const Eigen::Matrix<d
 	return coord_matrix.inverse();
 }
 
-Eigen::Vector3d fem::_3d::lambda(const Eigen::Vector3d& coords, const Eigen::Matrix<double, 4, 4>& simplex_coeff)
+Eigen::Vector4d fem::_3d::lambda(const Eigen::Vector3d& coords, const Eigen::Matrix<double, 4, 4>& simplex_coeff)
 {
 	Eigen::Vector4d x(coords(0), coords(1), coords(2), 1);
 	return simplex_coeff * x;
@@ -23,7 +23,7 @@ Eigen::Matrix<double, 4, 3> fem::_3d::nabla_lambda(const Eigen::Matrix<double, 4
 
 double fem::_3d::volume(const Eigen::Matrix<double, 4, 3>& coords)
 {
-	Eigen::Matrix<double, 3, 3> coord_matrix;
+	Eigen::Matrix<double, 4, 4> coord_matrix;
 	coord_matrix.col(0).setOnes();
 	coord_matrix.col(1) = coords.col(0);
 	coord_matrix.col(2) = coords.col(1);
