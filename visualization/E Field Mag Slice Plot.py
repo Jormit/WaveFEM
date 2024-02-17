7abf1a3d286ff56766a445e1708f313c410094d7
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-02-17 12:12:03 +1100

    Basic visualization in Python

---
+++ visualization/E Field Mag Slice Plot.py
@@ -0,0 +1,19 @@
+import numpy as np
+from mayavi import mlab
+
+import data_loader as dl
+
+x,y,z,vec = dl.import_3d_field("full_port_0_solution.txt", (30, 30, 30))
+vec_real = np.real(vec)
+vec_real_mag = np.linalg.norm(vec_real, axis=3)
+
+mlab.volume_slice(x, y, z, vec_real_mag, plane_orientation='x_axes')
+mlab.volume_slice(x, y, z, vec_real_mag, plane_orientation='y_axes')
+mlab.volume_slice(x, y, z, vec_real_mag, plane_orientation='z_axes')
+mlab.colorbar()
+mlab.outline()
+mlab.show()
+
+
+
+            
