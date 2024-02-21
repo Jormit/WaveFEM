bc8114076d79813661668cad91c5d9cf60bc4491
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-02-21 21:13:52 +1100

    Reformating of post evaluation

--- src/post.h
+++ src/post.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <memory>
+#include <Eigen/Dense>
+#include <utility>
+#include <vector>
 
 #include "sim.h"
 
@@ -7,12 +10,9 @@ enum slice_plane {XY, XZ, YZ};
 
 namespace post
 {
-	void eval_port(const sim& sim_instance, size_t num_x, size_t num_y);
-	void eval_port(const sim& sim_instance, size_t port_num, size_t mode, size_t num_x, size_t num_y);
+	std::vector<std::pair<Eigen::MatrixX2d, Eigen::MatrixX2cd>> eval_port(const sim& sim_instance, size_t num_x, size_t num_y);
+	std::pair<Eigen::MatrixX2d, Eigen::MatrixX2cd> eval_port(const sim& sim_instance, size_t port_num, size_t mode, size_t num_x, size_t num_y);
 
-	void eval_full(const sim& sim_instance, size_t num_x, size_t num_y, size_t num_z);
-	void eval_full(const sim& sim_instance, size_t port_num, size_t num_x, size_t num_y, size_t num_z);
-
-	void eval_slice(const sim& sim_instance, slice_plane slice, size_t num_u, size_t num_v, double w);
-	void eval_slice(const sim& sim_instance, slice_plane slice, size_t port_num, size_t num_u, size_t num_v, double w);
+	std::vector<std::pair<Eigen::MatrixX3d, Eigen::MatrixX3cd>> eval_full(const sim& sim_instance, size_t num_x, size_t num_y, size_t num_z);
+	std::pair<Eigen::MatrixX3d, Eigen::MatrixX3cd> eval_full(const sim& sim_instance, size_t port_num, size_t num_x, size_t num_y, size_t num_z);
 };
