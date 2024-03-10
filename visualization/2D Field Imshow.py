a8a83a21fadf92bf5da84b968bf5c16aa6303336
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-03-10 14:15:43 +1100

    Refactoring

--- visualization/2D Field Imshow.py
+++ visualization/2D Field Imshow.py
@@ -2,7 +2,7 @@ import data_loader as dl
 import matplotlib.pyplot as plt
 import numpy as np
 
-X, Y, vec = dl.import_2d_field("./Data/Horn Port 2d.txt")
+X, Y, vec = dl.import_2d_field("../examples/outputs/Horn Port 2d.txt")
 vec_real = np.real(vec)
 vec_real_mag = np.linalg.norm(vec_real, axis=2)
 
