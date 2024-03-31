b883541d6668d1188cbd7ed650bd77ddfc48982e
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-03-31 11:25:22 +1100

    Add initial stub for frontend app

--- visualization/utilities/3D Field Quiver Plot.py
+++ visualization/utilities/3D Field Quiver Plot.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import data_loader as dl
 
-x, y, z, vec = dl.import_3d_field("../examples/outputs/Waveguide 3d.txt")
+x, y, z, vec = dl.import_3d_field("./examples/outputs/Waveguide 3d.txt")
 vec_real = np.real(vec)
 
 mlab.quiver3d(x, y, z, vec_real[:,:,:,0], vec_real[:,:,:,1], vec_real[:,:,:,2])
