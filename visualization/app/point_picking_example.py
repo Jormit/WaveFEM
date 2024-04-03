a8c4e0c43a93b56fc9a0dfd65b33c680d310ddb9
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-04-03 21:19:57 +1100

    Surface picking instead of point picking

--- visualization/app/point_picking_example.py
+++ visualization/app/point_picking_example.py
@@ -1,31 +1,13 @@
 import pyvista as pv
+import numpy as np
 
-mesh = pv.Sphere()
+pl = pv.Plotter()
+cube = pv.Cube()
 
-pl = pv.Plotter(notebook=0)
-pl.add_mesh(mesh)
+def callback(point):
+    mouse_vector = np.subtract(point, pl.camera_position[0])
+    mouse_vector = mouse_vector / np.linalg.norm(mouse_vector)
 
-# Enable mouse tracking - access `pl.mouse_position`
-pl.track_mouse_position()
-
-# Enable point picking to improve picking results
-pl.enable_point_picking(show_message=False)
-
-def on_event(*args):
-    if pl.mouse_position is None:
-        return
-    x, y = pl.mouse_position
-    pl.add_text(f'{x}, {y}', name='mouse_position_text')
-    position = pl.pick_mouse_position()
-    print(position)
-    pl.add_point_labels([position], ['Mouse',], name='mouse_position_label', pickable=False)
-    
-# Timer callback - not a flushed out API and poor performance
-# pl.iren.add_observer("TimerEvent", on_event)
-# duration = 100  # milliseconds
-# pl.iren.create_timer(duration, repeating=True)
-
-# Mouse event callback, cleaner with better performance
-pl.iren.add_observer("MouseMoveEvent", on_event)
-
+pl.add_mesh(cube, show_edges=True)
+pl.enable_surface_point_picking(callback=callback, show_point=True, left_clicking=True)
 pl.show()
