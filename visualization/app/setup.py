9fa0354cea7e5df0b406a1364188adc1d379cb33
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-04-07 19:17:27 +1000

    Setup start

--- visualization/app/setup.py
+++ visualization/app/setup.py
@@ -1,7 +1,13 @@
 import json
 
 class setup:
+    def __init__(self):
+        self.ports = []
+        return
+
     def __init__(self, filename):
         f = open(filename)
         data = json.load(f)
         print(data)
+
+    
