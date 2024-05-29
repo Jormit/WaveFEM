23558a7bc1c82c5e63b4fbe78a4d190896984cb6
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2024-05-29 21:24:53 +1000

    Comment sim.cpp

--- src/sim.cpp
+++ src/sim.cpp
@@ -21,13 +21,19 @@ sim::sim(box bbox, double wavenumber, std::vector<material> materials, std::vect
 
 sim sim::create(sim_config config, std::string data_path)
 {
+	// Import step file
 	mesher_interface::initialize();
 	auto model_ids = mesher_interface::import_model(data_path + config.model_file);
 
+	// Create bounding box with user specified padding
 	auto boundary = mesher_interface::get_bounding_box();
 	boundary.add_padding(config.bounding_box_padding);
 	int boundary_id = mesher_interface::add_box(boundary);
 
+	// Generate conformal simulation volumes.
+	// This is handled by 2 cases:
+	// 1. Conductor objects are subtracted from bounding box and then removed.
+	// 2. Dielectric objects are substracted from bounding box and then kept.
 	std::vector<int> free_space_volumes{ boundary_id };
 	for (size_t i = 0; i < model_ids.size(); i++)
 	{
@@ -44,13 +50,14 @@ sim sim::create(sim_config config, std::string data_path)
 		}
 	}
 
+	// Create the PML if specified
 	pml_boundary pml;
-
 	if (config.pml_enable)
 	{
 		pml = pml::create(config.pml_thickness);
 	}
 
+	// Merge all conformal surfaces
 	mesher_interface::remove_duplicates();
 
 	mesher_interface::mesh_model(
@@ -58,9 +65,11 @@ sim sim::create(sim_config config, std::string data_path)
 		config.target_mesh_size * 1.1
 	);
 
+	// Create materials.
 	auto base_materials = mat::generate_base_set();
 	auto user_materials = mat::load_user_materials(config.materials);
 
+	// Get all nodes and boundary surfaces. Then mark which edges are on boundary.
 	auto nodes = mesher_interface::get_all_nodes();
 	auto boundaries = mesher_interface::get_boundary_surfaces();
 
@@ -71,6 +80,7 @@ sim sim::create(sim_config config, std::string data_path)
 	auto boundary_edge_map = helpers::set_to_map<size_t, int>(boundary_edges, BOUNDARY);
 	auto boundary_face_map = helpers::set_to_map<size_t, int>(boundary_faces, BOUNDARY);
 
+	// Get all elements and label material
 	auto elements = helpers::flatten_vector(mesher_interface::get_volume_elems(free_space_volumes));
 
 	if (config.pml_enable)
@@ -96,6 +106,7 @@ sim sim::create(sim_config config, std::string data_path)
 
 	base_materials.insert(base_materials.end(), user_materials.begin(), user_materials.end());
 
+	// Setup ports
 	ports ports(config.port_bounding_boxes, volume_material_map);
 	ports.setup_port_nodes_faces_edges(nodes, boundary_edge_map, boundary_face_map);
 
@@ -120,17 +131,19 @@ void sim::solve_ports()
 	port_dof_maps.clear();
 	for (int p = 0; p < sim_ports.num_ports(); p++)
 	{
+		// Generate port matrix
 		auto dof_map = fem::_2d::mixed_order::generate_dof_map(nodes, sim_ports.elements[p], boundary_edge_map);
 		port_dof_maps.push_back(dof_map);
-
 		Eigen::SparseMatrix<double> A;
 		Eigen::SparseMatrix<double> B;
 		std::tie(A, B) = fem::_2d::mixed_order::assemble_A_B(nodes, sim_ports.elements[p], materials, dof_map, wavenumber);
 
+		// Solve eigenproblem
 		Eigen::VectorXd values;
 		Eigen::MatrixXd vecs;
 		std::tie(values, vecs) = fem::solve_eigenproblem(A, -B, 1e-4);
 
+		// Save results
 		port_eigen_wave_numbers.push_back(values.cwiseSqrt());
 		port_eigen_vectors.push_back(vecs);
 
@@ -212,8 +225,10 @@ void sim::generate_outputs(std::string directory, sim_config config)
 		result_writer::write_structured_3d_field(directory + B_filename, h_field);
 	}
 
+	// Print s-params
 	auto s_params = post::eval_s_parameters(*this, 30, 30);
 	std::cout << s_params << std::endl;
 
+	// Save mesh
 	mesher_interface::write_vtk(directory + "mesh");
 }
